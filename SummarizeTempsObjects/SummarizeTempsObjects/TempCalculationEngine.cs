05dc94f8bba5c23547126f436589beeb457054be
llewandowski3 <59981831+llewandowski3@users.noreply.github.com>
2020-03-12 15:59:00 -0500

    check twice

--- SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
+++ SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
@@ -16,31 +16,82 @@ namespace SummarizeTempsObjects
         public int AverageTemperature()
         {
             // add logic to calculate the average
-            return 0; // <-- Replace this with the actual average
+
+            int sumOfTemps = 0;
+
+            // Loop through all the temps in the list
+
+            foreach (int temp in _temperatures)
+            {
+                // Add each temp to my sum variable
+                sumOfTemps += temp;
+                // sumOfTemps = sumOfTemps + temp;
+            }
+
+            // divide sum variable by the list count
+            int averageTemps = sumOfTemps / _temperatures.Count;
+            return averageTemps; 
+
         }
 
         public int TempsAboveThreshold(int threshold)
         {
             // add logic to calculate temps above the threshold
-            return 0; // <-- Replace this with the number
+
+            int Abovesum = 0;
+
+            foreach (int temp in _temperatures)
+            {
+                if (temp > threshold)
+                {
+                    Abovesum += 1;
+                }
+
+            }
+            return Abovesum; // <-- Replace this with the number
         }
 
         public int TempsBelowThreshold(int threshold)
         {
             // add logic to calculate and return temps below the threshold
-            return 0; // <-- Replace this with the number
+
+            int Belowsum = 0;
+
+            foreach (int temp in _temperatures)
+            {
+                if (temp < threshold)
+                {
+                    Belowsum += 1;
+                }
+
+            }
+            return Belowsum; // <-- Replace this with the number
+
         }
+
 
         public int TempsAtThreshold(int threshold)
         {
             // add logic to calculate and return temps at the threshold
-            return 0; // <-- Replace this with the number
+
+            int Atsum = 0;
+
+            foreach (int temp in _temperatures)
+            {
+                if (temp == threshold)
+                {
+                    Atsum += 1;
+                }
+
+            }
+            return Atsum; // <-- Replace this with the number
+
         }
 
         public int NumberOfTemperatures()
         {
             // add logic to return the number of temparature readings
-            return 0; // <-- Replace this with the number
+            return _temperatures.Count; // <-- Replace this with the number
         }
     }
 }
